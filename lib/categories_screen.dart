68a3f65263a34d37d5b2e1a7ae164b749211f6c8
utsav-baghela <utsavu858@gmail.com>
2023-07-11 14:00:04 +0530

    passing data via constructor

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,12 +7,13 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text ('DeliMeal'),
-      ),
+       ),
       body: GridView(
         padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
+                catData.id,
                 catData.title,
                 catData.color,
               ),
