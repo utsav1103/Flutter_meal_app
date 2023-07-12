7bb33b05e8963763e69c4bfc8b65018919f62023
utsav-baghela <utsavu858@gmail.com>
2023-07-12 20:11:20 +0530

    using routes and passing data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -16,6 +17,9 @@ class MyApp extends StatelessWidget {
         
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
