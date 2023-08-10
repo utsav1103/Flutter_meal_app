e6319465f5c0ab93714421ac303c22b11ad273bd
utsav-baghela <utsavu858@gmail.com>
2023-08-10 17:15:54 +0530

    replacing pages

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget{
+class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Text('Filters!'),
-    ); 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text(
+          'YOur Filters',
+        ),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filters!'),
+      ),
+    );
   }
 }
